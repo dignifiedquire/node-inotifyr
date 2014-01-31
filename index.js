07ae356e117be41366e41589aacd730d048a6e94
dignifiedquire <dignifiedquire@gmail.com>
2014-01-31 12:07:20 +0100

    Simple prototype works.

--- index.js
+++ index.js
@@ -1,6 +1,45 @@
 
+var inherits = require('util').inherits;
+var EventEmitter = require('events').EventEmitter;
+var path = require('path');
+
+var callbackToStream = require('read-stream/callback');
+var Inotify = require('inotify').Inotify;
+var through = require('through2');
 
 
-var Inotifyr = module.exports = function() {
+function watch(watcher, dir) {
+    return callbackToStream(function (cb) {
+        watcher.addWatch({
+            path: dir,
+            watch_for: Inotify.IN_CREATE,
+            callback: function (event) {
+                cb(null, event);
+            }
+        });
+    });
+}
+var Inotifyr = module.exports = function(dir, options) {
+    var self = this;
 
+    this._dir = dir;
+    this._watcher = new Inotify();
+    this._eventStream = watch(this._watcher, dir);
+    this._eventStream.on('data', function (data) {
+        if (data.mask && data.name) {
+            var mask = data.mask;
+            if (mask & Inotify.IN_CREATE) {
+                self.emit('add', path.join(self._dir, data.name), {
+                    isDir: !!(mask & Inotify.IN_ISDIR)
+                });
+            }
+        }
+    });
+};
+
+
+inherits(Inotifyr, EventEmitter);
+
+Inotifyr.prototype.close = function () {
+    this._watcher.close();
 };
--- test/index.spec.coffee
+++ test/index.spec.coffee
@@ -5,18 +5,27 @@ Inotifyr = require '../'
 
 describe 'inotifyr', ->
   beforeEach ->
-    fs.ensureDirSync './fixtures'
+    fs.ensureDirSync './test/fixtures'
 
   afterEach ->
-    fs.deleteDirSync './fixtures'
+    fs.deleteDirSync './test/fixtures'
 
-  it 'should watch a directory for add events', (done) ->
-    watcher = new Inotifyr 'fixtures'
+  it 'should watch a directory for file add events', (done) ->
+    watcher = new Inotifyr 'test/fixtures'
     watcher.on 'add', (filename, stats) ->
-      expect(filename).to.be.eql 'fixtures/new.txt'
+      expect(filename).to.be.eql 'test/fixtures/new.txt'
       expect(stats).to.have.property 'isDir', no
-      expect(stats).to.have.property 'mtime'
       watcher.close()
       done()
 
-    fs.createFileSync './fixtures/new.txt', 'hello world'
+    fs.createFileSync './test/fixtures/new.txt', 'hello world'
+
+  it 'should watch a directory for directory add events', (done) ->
+    watcher = new Inotifyr 'test/fixtures'
+    watcher.on 'add', (filename, stats) ->
+      expect(filename).to.be.eql 'test/fixtures/new'
+      expect(stats).to.have.property 'isDir', yes
+      watcher.close()
+      done()
+
+    fs.createDirSync './test/fixtures/new'
